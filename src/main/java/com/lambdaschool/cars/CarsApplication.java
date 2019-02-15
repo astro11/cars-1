49be897d37c986d8e4410a291c253d60fc2f3a4d
astro11 <aquila.strother@asu.edu>
2019-02-15 14:25:23 -0500

    created core CarsApplication with dependencies

--- src/main/java/com/lambdaschool/cars/CarsApplication.java
+++ src/main/java/com/lambdaschool/cars/CarsApplication.java
@@ -1,15 +1,16 @@
 package com.lambdaschool.cars;
 
-import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class CarsApplication
